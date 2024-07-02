3cb395eca5926a470d7384a47ecb3dc8c2b933d2
ethfal <ethfalrabiul2106@gmail.com>
2024-07-02 16:02:42 +0700

    first commit

---
+++ application/controllers/contoh01.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh01 extends CI_Controller
+{
+    public function index()
+    {
+        echo"<h1>Perkenalkan</h1>";
+        echo"Nama saya genta
+        saya tinggal didepok";
+    }
+}
